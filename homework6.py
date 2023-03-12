f794a470b6e056dbea8b7e13a8fa42085f6cf783
unknown <Andrew M>
2023-03-12 13:25:04 +0300

    The task 32 is done

---
+++ homework6.py
@@ -0,0 +1,36 @@
+# Задача 30:  Заполните массив элементами арифметической прогрессии. 
+# Её первый элемент, разность и количество элементов нужно ввести 
+# с клавиатуры. Формула для получения n-го члена прогрессии: an = a1 + (n-1) * d.
+# Каждое число вводится с новой строки.
+
+
+
+
+
+
+# Задача 32: Определить индексы элементов массива (списка), значения 
+# которых принадлежат заданному диапазону (т.е. не меньше заданного минимума 
+# и не больше заданного максимума)
+# Ввод:  [-5, 9, 0, 3, -1, -2, 1, 4, -2, 10, 2, 0, -9, 8, 10, -9, 0, -5, -5, 7]
+# 5
+# 15
+# Вывод: [1, 9, 13, 14, 19]
+
+import random
+
+
+def define(arr, mi, ma):
+    res = []
+    for i in range(len(array)):
+        if arr[i] in range(mi, ma):
+            res.append(i)
+
+    return res
+           
+
+size = int(input("Enter the length of the array: "))
+array = [random.randint(-999, 1000) for i in range(size)]
+print(array)
+min = int(input("Enter the minimal number of the range: "))
+max = int(input("Enter the maximal number of the range: "))
+print(f"The elements of the definde range have following indexes {define(array, min, max)}")
