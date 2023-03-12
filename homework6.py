bd233296dac4a9895136d1eeca93ba92c721bd24
unknown <Andrew M>
2023-03-12 13:44:52 +0300

    the task 30 is done

--- homework6.py
+++ homework6.py
@@ -4,6 +4,13 @@
 # Каждое число вводится с новой строки.
 
 
+# first = int(input("Enter the first number: \n"))
+# amount = int(input("Enter the amount of elements: \n"))
+# difference = int(input("Enter the difference: \n"))
+# array = [first]
+# for i in range(1, amount-1):
+#     array.append(first + ((i-1) * difference))
+# print(array)
 
 
 
@@ -16,21 +23,21 @@
 # 15
 # Вывод: [1, 9, 13, 14, 19]
 
-import random
+# import random
 
 
-def define(arr, mi, ma):
-    res = []
-    for i in range(len(array)):
-        if arr[i] in range(mi, ma):
-            res.append(i)
+# def define(arr, mi, ma):
+#     res = []
+#     for i in range(len(array)):
+#         if arr[i] in range(mi, ma):
+#             res.append(i)
 
-    return res
+#     return res
            
 
-size = int(input("Enter the length of the array: "))
-array = [random.randint(-999, 1000) for i in range(size)]
-print(array)
-min = int(input("Enter the minimal number of the range: "))
-max = int(input("Enter the maximal number of the range: "))
-print(f"The elements of the definde range have following indexes {define(array, min, max)}")
+# size = int(input("Enter the length of the array: "))
+# array = [random.randint(-999, 1000) for i in range(size)]
+# print(array)
+# min = int(input("Enter the minimal number of the range: "))
+# max = int(input("Enter the maximal number of the range: "))
+# print(f"The elements of the definde range have following indexes {define(array, min, max)}")
